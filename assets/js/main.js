3aed2131e9abb43815ad9dfa3945510cce8afbc4
woodraven <social@adamraven.com>
2023-02-03 17:00:28 -0500

    final commit feb323

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,10 +203,10 @@
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
-    speed: 400,
+    speed: 500,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 3000,
       disableOnInteraction: false
     },
     pagination: {
